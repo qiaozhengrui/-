217c6ffdf7b8d67832fc5dba2e27344cb492c0e1
Joe <365906799@qq.com>
2022-10-21 23:21:53 +0800

    新增了添加到购物车成功组件

--- src/pages/AddCartSuccess/index.vue
+++ src/pages/AddCartSuccess/index.vue
@@ -5,16 +5,19 @@
       <div class="goods">
         <div class="left-good">
           <div class="left-pic">
-            <img src="good.skuDefaultImg">
+            <img :src="skuInfo.skuDefaultImg">
           </div>
           <div class="right-info">
-            <p class="title">小米红米 Redmi note8 手机 梦幻蓝 全网通(4GB+64GB)</p>
-            <p class="attr">颜色：WFZ5099IH/5L钛金釜内胆 数量：2</p>
+            <!-- 商品的名字 -->
+            <p class="title">{{ skuInfo.skuName}}</p>
+            <!-- 商品的详情 -->
+            <p class="attr">{{ skuInfo.skuDesc}} 数量：{{ $route.query.skuNum }}</p>
           </div>
         </div>
         <div class="right-gocart">
-          <a href="javascript:" class="sui-btn btn-xlarge">查看商品详情</a>
-          <a href="javascript:" >去购物车结算 > </a>
+          <!-- 把产品id带回detail -->
+          <router-link :to="`/detail/${skuInfo.id}`" class="sui-btn btn-xlarge">查看商品详情</router-link>
+          <router-link to="/shopcart">去购物车结算</router-link>
         </div>
       </div>
     </div>
@@ -24,6 +27,11 @@
 <script>
   export default {
     name: 'AddCartSuccess',
+    computed: {
+      skuInfo() {
+        return JSON.parse(sessionStorage.getItem('SKUINFO'))
+      }
+    },
   }
 </script>
 
