84a96f19e80728ae6827773ea9b39dd808cb69df
Joe <365906799@qq.com>
2022-10-21 23:44:55 +0800

    引入了detaile和shopcart两个子仓库

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,7 @@ Vue.use(Vuex)
 import home from './home'
 import search from './search'
 import detail from './detail'
+import shopcart from './shopcart'
 
 //对外暴露Store类的一个实例
 export default new Vuex.Store({
@@ -16,5 +17,6 @@ export default new Vuex.Store({
     home,
     search,
     detail,
+    shopcart,
   }
 })
