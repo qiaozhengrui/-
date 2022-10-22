6f57cc051f56b3d15495ef56682251b6c4e6d74a
Joe <365906799@qq.com>
2022-10-22 18:13:25 +0800

    把原先的pages文件夹更名为views

---
+++ src/views/Home/Floor/index.vue
@@ -0,0 +1,211 @@
+<template>
+  <!--楼层-->
+  <div class="floor">
+    <div class="py-container">
+      <div class="title clearfix">
+        <h3 class="fl">{{ list.name }}</h3>
+        <div class="fr">
+          <ul class="nav-tabs clearfix">
+            <!-- 热门，大家电，生活电器等 -->
+            <li v-for="(nav, index) in list.navList" :key="nav.index">
+              <a href="#tab1" data-toggle="tab">{{ nav.text }}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="tab-content">
+        <div class="tab-pane">
+          <div class="floor-1">
+            <div class="blockgary">
+              <ul class="jd-list">
+                <li v-for="(keyword, index) in list.keywords" :key="index">{{ keyword }}</li>
+              </ul>
+              <img :src="list.imgUrl" />
+            </div>
+            <div class="floorBanner">
+              <!-- 轮播图的地方 -->
+              <Carousel :list="list.carouselList" />
+            </div>
+            <div class="split">
+              <span class="floor-x-line"></span>
+              <div class="floor-conver-pit">
+                <img :src="list.recommendList[0]" />
+              </div>
+              <div class="floor-conver-pit">
+                <img :src="list.recommendList[1]" />
+              </div>
+            </div>
+            <div class="split center">
+              <img :src="list.bigImg" />
+            </div>
+            <div class="split">
+              <span class="floor-x-line"></span>
+              <div class="floor-conver-pit">
+                <img :src="list.recommendList[2]" />
+              </div>
+              <div class="floor-conver-pit">
+                <img :src="list.recommendList[3]" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "",
+  props: ["list"],
+  //组件挂载完毕的地方
+  mounted() {
+    //问：开始书写Swiper时，在mounted中不可书写，现在可以书写？
+    //开始写轮播图时，是在当前组件内部发请求、动态渲染结构(前提服务器数据至少回来)，因此之前写法(watch+$nextTick)在这不行
+    //现在可以是因为请求是父组件发的，父组件通过props传递过来的，且结构都已有，才执行mounted
+  },
+};
+</script>
+
+<style scoped lang="less">
+.floor {
+  margin-top: 15px;
+
+  .py-container {
+    width: 1200px;
+    margin: 0 auto;
+
+    .title {
+      height: 50px;
+      .fl {
+        float: left;
+        color: #c81623;
+        font-size: 20px;
+        line-height: 30px;
+        margin: 9px 0;
+        font-weight: 700;
+
+      }
+
+      .fr {
+        float: right;
+
+        .nav-tabs {
+          margin: 10px 0 0;
+          display: inline-block;
+
+          li {
+            list-style: none;
+            float: left;
+            line-height: 18px;
+
+            a {
+              padding-top: 1px;
+              font-weight: 400;
+              background-color: #fff;
+
+              &::after {
+                content: "|";
+                padding: 0 10px;
+              }
+            }
+
+            &:nth-child(7) {
+              a {
+                &::after {
+                  content: "";
+                }
+              }
+            }
+
+            &.active {
+              a {
+                color: #e1251b;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    .tab-content {
+      border-top: 2px solid #c81623;
+      border-bottom: 1px solid #e4e4e4;
+
+      .tab-pane {
+        .floor-1 {
+          height: 360px;
+          display: flex;
+
+          .blockgary {
+            width: 210px;
+            height: 100%;
+            background: #f7f7f7;
+
+            .jd-list {
+              padding: 15px 0;
+              overflow: hidden;
+              // display: flex;
+
+              li {
+                list-style-type: none;
+                float: left;
+                width: 40%;
+                margin: 0 10px;
+                border-bottom: 1px solid #e4e4e4;
+                text-align: center;
+                line-height: 26px;
+              }
+            }
+
+            img {
+              // width: 100%;
+              height: 200px;
+            }
+          }
+
+          .floorBanner {
+            width: 330px;
+            height: 100%;
+          }
+
+          .split {
+            width: 220px;
+            height: 100%;
+            position: relative;
+
+            .floor-x-line {
+              position: absolute;
+              background: #e4e4e4;
+              width: 220px;
+              height: 1px;
+              top: 180px;
+            }
+
+            .floor-conver-pit {
+              width: 100%;
+              height: 50%;
+
+              img {
+                width: 100%;
+                height: 100%;
+                transition: all 400ms;
+
+                &:hover {
+                  opacity: 0.8;
+                }
+              }
+            }
+          }
+
+          .center {
+            border: 1px solid #e4e4e4;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,51 @@
+<!-- 路由组件 -->
+<template>
+  <div>
+    <!-- 三级联动全局组件：三级联动已注册为全局组件，因此不需要在引入 -->
+    <TypeNav />
+    <!-- 其他组件 -->
+    <ListContainer />
+    <Recommend />
+    <Rank />
+    <Like />
+    <!-- Floor组件内部是没有发请求的，数据是父组件传过来的 -->
+    <Floor v-for="(floor, index) in floorList" :key="floor.id" :list="floor" />
+    <Brand />
+  </div>
+</template>
+
+<script>
+//引入其余的组件。注意：其他组件不是全局组件，需要引入注册使用
+import ListContainer from '@/views/Home/ListContainer'
+import Recommend from '@/views/Home/Recommend'
+import Rank from '@/views/Home/Rank'
+import Like from '@/views/Home/Like'
+import Floor from '@/views/Home/Floor'
+import Brand from '@/views/Home/Brand'
+import { mapState } from 'vuex'
+
+export default {
+    name: "Home",
+    components: {
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Floor,
+    Brand,
+  },
+  mounted() {
+    //派发action，获取floor组件的数据      发请求
+    this.$store.dispatch("getFloorList")
+  },
+  computed: {
+    ...mapState({
+      floorList: state => state.home.floorList
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
