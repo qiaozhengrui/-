11e3bec50a8f374d8e4df469a32c983b8b821f7f
Joe <365906799@qq.com>
2022-10-25 15:15:06 +0800

    新增了交易页面(trade)子仓库以及 获取用户地址信息和获取交易页面商品清单的信息的异步操作

---
+++ src/store/trade/index.js
@@ -0,0 +1,42 @@
+//结算交易的仓库
+import { reqAddressInfo, reqOrderInfo } from '@/api'
+
+const state = {
+  address: [],
+  orderInfo: {},
+}
+const mutations = {
+  GETUSERADDRESS(state, address) {
+    state.address = address
+  },
+  GETORDERINFO(state, orderInfo) {
+    state.orderInfo = orderInfo
+  }
+}
+const actions = {
+  //获取用户地址信息
+  async getUserAddress({commit}) {
+    let result = await reqAddressInfo()
+    if (result.code == 200) {
+      commit('GETUSERADDRESS', result.data)
+    }
+  },
+  //获取交易页面商品清单的信息
+  async getOrderInfo({commit}) {
+    let result = await reqOrderInfo()
+    console.log(result)
+    if (result.code == 200) {
+      commit('GETORDERINFO', result.data)
+    }
+  }
+}
+const getters = {
+
+}
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters,
+}
